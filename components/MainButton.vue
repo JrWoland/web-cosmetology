18dc8656c9642812b31f03154f259aa85169a836
Marcin Kozicki <m.kozicki2@gmail.com>
2020-02-05 20:58:30 +0100

    Add button component

---
+++ components/MainButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="main-button">
+    {{ buttonContent }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'MainButton',
+  props: {
+    buttonContent: {
+      type: String,
+      default: 'Default'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main-button {
+  font-family: $second-font, sans-serif;
+  font-size: $medium-font;
+  font-weight: 300;
+  padding: 0.3em 2.5em;
+  border-radius: 10em;
+  border: none;
+  outline: none;
+  background-color: $attention-color;
+  color: white;
+  text-transform: uppercase;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: darken($attention-color, 10%);
+    cursor: pointer;
+  }
+}
+</style>
