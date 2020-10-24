403fde79443fbc3d9d0c275eaef80ceccc34eb2b
Marcin Kozicki <m.kozicki2@gmail.com>
2020-10-24 14:21:56 +0200

    Netlify test3

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -7,7 +7,7 @@
       </h1>
       <p class="hero-section__subtitle">
         Together creeping heaven upon
-        <br />third dominion be develop
+        <br />third dominion be develop3
       </p>
       <div class="hero-section__button">
         <MainButton button-content="services" />
