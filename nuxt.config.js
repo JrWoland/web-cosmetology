b4f8434e4fcb86f20adda5193126502c74e47ffb
Marcin Kozicki <m.kozicki2@gmail.com>
2022-08-29 12:21:35 +0200

    Add GA id

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,7 +87,12 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    {
+      src: './plugins/GoogleAnalytics.js',
+      mode: 'client'
+    }
+  ],
   /*
    ** Nuxt.js dev-modules
    */
