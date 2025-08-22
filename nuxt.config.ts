68d1dc643aec772ce9a3389dd551457cd486e235
Marcin Kozicki <m.kozicki2@gmail.com>
2025-08-22 22:23:07 +0200

    Add Netlify image provider configuration and update .gitignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   pages: true,
+  image: {
+    provider: 'netlify'
+  },
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image', 'nuxt-aos'],
   vite: {
     css: {
